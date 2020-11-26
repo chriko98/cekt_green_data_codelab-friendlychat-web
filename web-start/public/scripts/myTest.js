8e9ef26960b071f34aacd9ad7ab109b611a57c42
chriko98 <62097031+chriko98@users.noreply.github.com>
2020-11-26 17:53:34 +0100

    DONT_HOST_erste_abrufe_db
    NICHT ONLINE HOSTEN!!!
    
    security rules wurden "deaktiviert" zum entwickeln am localhost

--- web-start/public/scripts/myTest.js
+++ web-start/public/scripts/myTest.js
@@ -1,20 +1,26 @@
 console.log("hellothere");
 
-var mydiv = document.getElementById("myplantdiv");
-var magic = document.getElementById("magic-button");
 
-function doMagic(){
-    console.log("MAGIC!!!");
-    mydiv.append("M-a-g-i-c");
-}
+// < - - - - METHODS - - - - >
+// function signIn() {
+//     // Sign into Firebase using popup auth & Google as the identity provider.
+//     var provider = new firebase.auth.GoogleAuthProvider();
+//     firebase.auth().signInWithPopup(provider);
+// }
 
 
-function addPlant(){
+// getPlant("Palme");
+// getPlant("The Fairy");
+
+function addPlant(id, familie, gebrauchsname, hoehe_m, standort, typ, wasserbedarf_woche){
     // Add a new document in collection "cities"
-    firebase.firestore().collection("data").doc("LA").set({
-        name: "Los Angeles",
-        state: "CA",
-        country: "USA"
+    firebase.firestore().collection("plants").doc(id).set({
+        familie: familie,
+        gebrauchsname: gebrauchsname,
+        hoehe_m: hoehe_m,
+        standort: standort,
+        typ: typ,
+        wasserbedarf_woche: wasserbedarf_woche
     })
         .then(function() {
             console.log("Document successfully written!");
@@ -24,5 +30,95 @@ function addPlant(){
         });
 }
 
+function addSchlafmohn(){
+    var docRef = firebase.firestore().collection("plants").doc("Schlafmohn");
+    console.log("- - - - - ");
+    console.log(docRef);
+    docRef.get().then(function(doc) {
+        if (doc.exists) {
+            console.log("DOKUMENT EXISTIERT SCHON - KEIN ADD!! Daten:")
+            console.log("Document ID:", doc.id)
+            console.log("Document data:", doc.data());
+            console.log("- - - - - ");
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("- - - - -");
+            console.log("DOKUMENT EXISTIERT NOCH NICHT. Daten werden gespeichert...");
+            docRef.set({
+                familie: "Mohngewächse",
+                gebrauchsname: "Mohn",
+                hoehe_m: "1",
+                standort: "Natur",
+                typ: "Nutzpflanze",
+                wasserbedarf_woche: "0"
+            })
+                .then(function() {
+                    console.log("Document successfully written!");
+                    console.log("- - - - -");
+                });
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
+
+
+
+
+
+
+
+}
+
+
+function getPlant(id) {
+    var docRef = firebase.firestore().collection("plants").doc(id);
+    console.log(docRef);
+    docRef.get().then(function(doc) {
+        if (doc.exists) {
+            console.log("THIS PLANT EXISTS!")
+            console.log("Document ID:", doc.id)
+            console.log("Document data:", doc.data());
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("NO SUCH DOCUMENT!");
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
+}
+
+
+
+
+
+function getHundsrose() {
+    var docRef = firebase.firestore().collection("plants").doc("Hundsrose");
+    console.log(docRef);
+    docRef.get().then(function(doc) {
+        if (doc.exists) {
+            console.log("Document ID:", doc.id)
+            console.log("Document data:", doc.data());
+            console.log("I am so happy!");
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
+}
+
+
+// < - - - - VARIABLES - - - - >
+var mydiv = document.getElementById("myplantdiv");
+var v_getplant = document.getElementById("get-plant-button");
+var v_saveSchlafmohn = document.getElementById("schlafmohn-button");
+// var signInButtonElement = document.getElementById('sign-in');
+
+
+//< - - - - EVENT-LISTENERS - - - - - >
 mydiv.append("meine bilder, vermutlich ein img tag oder so");
-magic.addEventListener('click', addPlant);
+v_getplant.addEventListener('click', getHundsrose);
+v_saveSchlafmohn.addEventListener("click", addSchlafmohn);
+// signInButtonElement.addEventListener('click', signIn);
+
