37d4543ea2732622a28ba4558c6b4ee457989d2a
chriko98 <62097031+chriko98@users.noreply.github.com>
2020-11-26 14:47:26 +0100

    changes15:00

--- web-start/public/scripts/myTest.js
+++ web-start/public/scripts/myTest.js
@@ -1,5 +1,28 @@
 console.log("hellothere");
 
 var mydiv = document.getElementById("myplantdiv");
+var magic = document.getElementById("magic-button");
+
+function doMagic(){
+    console.log("MAGIC!!!");
+    mydiv.append("M-a-g-i-c");
+}
+
+
+function addPlant(){
+    // Add a new document in collection "cities"
+    firebase.firestore().collection("data").doc("LA").set({
+        name: "Los Angeles",
+        state: "CA",
+        country: "USA"
+    })
+        .then(function() {
+            console.log("Document successfully written!");
+        })
+        .catch(function(error) {
+            console.error("Error writing document: ", error);
+        });
+}
 
 mydiv.append("meine bilder, vermutlich ein img tag oder so");
+magic.addEventListener('click', addPlant);
